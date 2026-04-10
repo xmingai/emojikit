dfed32f75d5c01af3b4e35264d5e8300cd42644c
xmingai <truiosacer543@gmail.com>
2026-04-10 17:55:10 +0800

    ui: move toast notifications to top-right corner

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -9,6 +9,7 @@ const Toaster = ({ ...props }: ToasterProps) => {
 
   return (
     <Sonner
+      position="top-right"
       theme={theme as ToasterProps["theme"]}
       className="toaster group"
       icons={{
